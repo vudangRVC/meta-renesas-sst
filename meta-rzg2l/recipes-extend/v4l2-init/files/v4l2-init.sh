b9ca1447c7b6596352ff54eb9bba8d9b7ac47a45
sonnguyencongbv <son.nguyen-cong@banvien.com.vn>
2024-01-09 15:14:36 +0700

    meta-rzg2l: recipes-extend: v4l2-init: Fix script init ov5640
    Signed-off-by: sonnguyencongbv <son.nguyen-cong@banvien.com.vn>
    Signed-off-by: Vu Dang <vu.dang.te@renesas.com>

--- meta-rzg2l/recipes-extend/v4l2-init/files/v4l2-init.sh
+++ meta-rzg2l/recipes-extend/v4l2-init/files/v4l2-init.sh
@@ -3,11 +3,10 @@
 cru=$(cat /sys/class/video4linux/video*/name | grep "CRU")
 csi2=$(cat /sys/class/video4linux/v4l-subdev*/name | grep "csi2")
 
-# Available resolution of OV5645.
+# Available resolution of OV5640.
 # Please choose one of a following resolution then comment out the rest.
-#ov5645_res=1280x960
-ov5645_res=1920x1080
-#ov5645_res=2592x1944
+#ov5640_res=1280x960
+ov5640_res=1920x1080
 
 if [ -z "$cru" ]
 then
@@ -19,8 +18,8 @@ else
 		echo "No MIPI CSI2 sub video device founds"
 	else
 		media-ctl -d /dev/media0 -l "'rzg2l_csi2 10830400.csi2':1 -> 'CRU output':0 [1]"
-		media-ctl -d /dev/media0 -V "'rzg2l_csi2 10830400.csi2':1 [fmt:UYVY8_2X8/$ov5645_res field:none]"
-		media-ctl -d /dev/media0 -V "'ov5645 0-003c':0 [fmt:UYVY8_2X8/$ov5645_res field:none]"
-		echo "Link CRU/CSI2 to ov5645 0-003c with format UYVY8_2X8 and resolution $ov5645_res"
+		media-ctl -d /dev/media0 -V "'rzg2l_csi2 10830400.csi2':1 [fmt:UYVY8_2X8/$ov5640_res field:none]"
+		media-ctl -d /dev/media0 -V "'ov5640 1-003c':0 [fmt:UYVY8_2X8/$ov5640_res field:none]"
+		echo "Link CRU/CSI2 to ov5640 1-003c with format UYVY8_2X8 and resolution $ov5640_res"
 	fi
 fi
